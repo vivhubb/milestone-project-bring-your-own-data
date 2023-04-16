60ec5f78338f30e244297cf1060ee88df30256ac
vivhubb <vivienne.2214@yahoo.com>
2023-04-16 15:00:52 +0000

    adds template for cloud IDE

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
-
-def page_summary():
-    st.info('string')
+from app_pages.page_project_summary import project_summary
 
 app = MultiPage(app_name='Price Predictor')
 
-app.add_page('Quick Project Summary', page_summary)
+app.add_page('Quick Project Summary', project_summary)
 
 app.run()
---
+++ app_pages/page_project_summary.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def project_summary():
+    st.title('Quick Project Summary')
+    st.info(
+        f'**General information**'
+        f'**Project Dataset**'
+    )
