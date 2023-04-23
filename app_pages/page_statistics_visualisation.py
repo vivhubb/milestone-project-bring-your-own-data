15f274a4267fd88f2a2c5a8993974c9448b758f4
vivienne <vivienne.2214@yahoo.com>
2023-04-23 14:23:12 +0000

    add checkboxes to Visualisations page

--- app_pages/page_statistics_visualisation.py
+++ app_pages/page_statistics_visualisation.py
@@ -8,31 +8,31 @@ def statistics_visualisation():
 
     st.write('## Visualisations')
 
-    st.write('#### Correlation Matrix')
-    st.write(corr_matrix_heatmap())
+    if st.checkbox('**Correlation Matrix**'):
+        st.write(corr_matrix_heatmap())
 
-    st.write('#### Dataframe used for below visualisations')
+    st.write('**Dataframe used for below visualisations**')
     st.dataframe(df)
 
-    st.write('#### Selling price and KM driven per year')
-    st.plotly_chart(visualisation_pky(df))
+    if st.checkbox('**Selling price and KM driven per year**'):
+        st.plotly_chart(visualisation_pky(df))
 
-    st.write('#### Selling price by owner count')
-    st.plotly_chart(visualisation_po(df))
+    if st.checkbox('**Selling price by owner count**'):
+        st.plotly_chart(visualisation_po(df))
 
-    st.write('#### Selling Price by Owner Count and Year')
-    st.plotly_chart(visualisation_pocy(df))
+    if st.checkbox('**Selling Price by Owner Count and Year**'):
+        st.plotly_chart(visualisation_pocy(df))
 
-    st.write('#### Identifying outliers')
-    st.write('* **Selling Price by Year**')
-    st.plotly_chart(visualisation_outliers(df, x='year', y='selling_price'))
-    st.write('* **Selling Price by KMs driven**')
-    st.plotly_chart(visualisation_outliers(df, x='km_driven', y='selling_price'))
+    if st.checkbox('**Identifying outliers**'):
+        st.write('* **Selling Price by Year**')
+        st.plotly_chart(visualisation_outliers(df, x='year', y='selling_price'))
+        st.write('* **Selling Price by KMs driven**')
+        st.plotly_chart(visualisation_outliers(df, x='km_driven', y='selling_price'))
 
 
-    st.write('#### **Dataframe used for below visualisation**')
+    st.write('**Dataframe used for below visualisation**')
     df = calculate_price_difference(df)
     st.dataframe(df)
 
-    st.write('#### **How KMs driven affect price drop**')
-    st.plotly_chart(visualisation_kmpd(df))
+    if st.checkbox('**How KMs driven affect price drop**'):
+        st.plotly_chart(visualisation_kmpd(df))
